8e76f6d79922e351c9c0e1d31113ec13b9650907
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-07-02 15:15:19 +0500

    simple example

--- main.cxx
+++ main.cxx
@@ -1,14 +1,22 @@
-#include <iostream>
 #include <gtkmm/application.h>
-#include <main_window.h>
+#include <gtkmm/window.h>
+
+class MyWindow : public Gtk::Window
+{
+  public:
+    MyWindow()
+    {
+        set_title("Basic Application");
+        set_default_size(200, 200);
+        set_default_icon_name("text-x-generic");
+    }
+};
 
 int main(int argc, char **argv)
 {
     //std::cout<< "It works!" << std::endl;
     
-    auto app = Gtk::Application::create("org.example.demo");
+    auto app = Gtk::Application::create("org.gtkmm.examples.base");
 
-    ExampleWindow examplewindow;
-    
-    return app->run(examplewindow);
+    return app->make_window_and_run<MyWindow>(argc, argv);
 }
