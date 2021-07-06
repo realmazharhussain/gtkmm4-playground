bf2d057999d0131c280f7a83185949ecd16d4964
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-07-06 11:52:40 +0500

    Radio Buttons

--- include/mainWindow.h
+++ include/mainWindow.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <gtkmm/window.h>
-#include <gtkmm/togglebutton.h>
+#include <gtkmm/button.h>
+#include <gtkmm/checkbutton.h>
 #include <gtkmm/image.h>
 #include <gtkmm/label.h>
 #include <gtkmm/box.h>
+#include <gtkmm/separator.h>
 
 class MainWindow : public Gtk::Window
 {
@@ -13,8 +15,16 @@ class MainWindow : public Gtk::Window
 
   protected:
     // signal handlers
-    void on_togglebutton_toggled();
+    void on_button_quit_clicked();
+    void on_radiobuttons_toggled();
 
     // child widgets
-    Gtk::ToggleButton     m_togglebutton;
+    Gtk::CheckButton      m_RadioButton1,
+                          m_RadioButton2,
+                          m_RadioButton3;
+    Gtk::Box              m_Box_Main,
+                          m_Box_RadioButtons,
+                          m_Box_QuitButton;
+    Gtk::Separator        m_Separator;
+    Gtk::Button           m_Button_Quit;
 };
--- src/mainWindow.cxx
+++ src/mainWindow.cxx
@@ -3,23 +3,64 @@
 
 MainWindow::MainWindow()
 :
-m_togglebutton("something")
+m_RadioButton1("Option 1"),
+m_RadioButton2("Option 2"),
+m_RadioButton3("Option 3"),
+m_Box_Main(Gtk::Orientation::VERTICAL),
+m_Box_RadioButtons(Gtk::Orientation::VERTICAL),
+m_Box_QuitButton(Gtk::Orientation::VERTICAL),
+m_Button_Quit("Quit")
 {
-        set_title("Toggle Button");
+        set_title("Radio Buttons");
         // set_default_size(200, 200);
-        set_default_icon_name("text-x-generic");
+        // set_default_icon_name("text-x-generic");
 
-        m_togglebutton.set_margin(10);
-        m_togglebutton.signal_toggled().connect(sigc::mem_fun(*this, &MainWindow::on_togglebutton_toggled));
-        set_child(m_togglebutton);
+        // Set Margin for Container Boxes
+        m_Box_RadioButtons.set_margin(10);
+        m_Box_QuitButton.set_margin(10);
+
+        // Ordering Container Widgets
+        m_Box_Main.append(m_Box_RadioButtons);
+        m_Box_Main.append(m_Separator);
+        m_Box_Main.append(m_Box_QuitButton);
+
+        // Grouping RadioButtons together
+        m_RadioButton2.set_group(m_RadioButton1);
+        m_RadioButton3.set_group(m_RadioButton1);
+
+        // Selecting RadioButton2 by default
+        m_RadioButton2.set_active(true);
+
+        // Filling m_Box_RadioButtons with RadioButtons
+        m_Box_RadioButtons.append(m_RadioButton1);
+        m_Box_RadioButtons.append(m_RadioButton2);
+        m_Box_RadioButtons.append(m_RadioButton3);
+
+        // Filling m_Box_QuitButton with m_Button_Quit
+        m_Box_QuitButton.append(m_Button_Quit);
+
+        // Connect Signal Handlers
+        m_Button_Quit.signal_clicked().connect(sigc::mem_fun(*this, &MainWindow::on_button_quit_clicked));
+        // m_RadioButton1.signal_toggled().connect(sigc::mem_fun(*this, &MainWindow::on_radiobuttons_toggled));
+        // m_RadioButton2.signal_toggled().connect(sigc::mem_fun(*this, &MainWindow::on_radiobuttons_toggled));
+        // m_RadioButton3.signal_toggled().connect(sigc::mem_fun(*this, &MainWindow::on_radiobuttons_toggled));
+
+        // Finally, Add Main Container Box to the window
+        set_child(m_Box_Main);
 }
 
 MainWindow::~MainWindow()
 {
 }
 
-void MainWindow::on_togglebutton_toggled()
+
+void MainWindow::on_button_quit_clicked()
 {
-    std::cout<< (m_togglebutton.get_active()?"enabled":"disabled")
-             << std::endl;
+    hide();
 }
+
+/*
+void MainWindow::on_radiobuttons_toggled()
+{
+}
+*/
